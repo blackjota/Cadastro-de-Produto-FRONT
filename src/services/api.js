a1d049a2af4299c8223acc81980dcbce54cc91a2
joaotostesorg <joao.tostes@orgsystem.com.br>
2020-06-15 02:15:30 -0300

    Heroku configs

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://localhost:5001/api/v1.0",
+  baseURL: "https://cadastro-de-produtos-api.herokuapp.com/api/v1.0",
 });
 
 export default api;
